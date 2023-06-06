ada63494fa41e242a0828d2baa6603490e375703
JulianGabo06 <juliangabo1204@gmail.com>
2023-06-06 11:45:16 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var boton1 = document.getElementById("boton1");
+var boton2 = document.getElementById("boton2");
+var boton3 = document.getElementById("boton3");
+
+boton1.addEventListener("click", function() {
+  alert("Haz hecho click en el botón 1");
+});
+
+boton2.addEventListener("click", function() {
+  alert("Haz hecho click en el botón 2");
+});
+
+boton3.addEventListener("click", function() {
+  alert("Haz hecho click en el botón 3");
+});
